3f23b9210937102a98b44181f8a179e4216d3726
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-03 18:00:59 -0700

    Handles User Input

--- play.js
+++ play.js
@@ -2,6 +2,26 @@ const net = require('net');
 const connect = require('./client')
 
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+const handleUserInput = function () {
+  // on any input from stdin (standard input), output a "." to stdout
+  stdin.on('data', (key) => {
+  process.stdout.write('.');
+  if (key === '\u0003') {
+    process.exit();
+  }
+});
+};
 
 console.log('Connecting ...');
 connect();
